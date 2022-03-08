2e9fa13b288612c75ad58d8c6f921499d53befba
PixelVengeur <65874464+PixelVengeur@users.noreply.github.com>
2022-03-08 11:05:30 +0100

    Â

--- Sans titre1.ps1
+++ Sans titre1.ps1
@@ -21,22 +21,21 @@ PLAN
 #> 
 
 # VIDEO
-$video = ".\videos\Akebi-chan no Sailor-fuku - S01E07 - VOSTFR 1080p WEB x264 -NanDesuKa (WAKA).mkv"
-$image = ".\img4.jpg"
+$image = "C:\Users\Nathan\OneDrive\Images\Epic-Handshake.jpg"
 $testing = 1
 
 # region Lister toutes les vidéos
-# Clear-Content .\videos.txt
-# foreach ($file in Get-ChildItem .\videos\* -Include @("*.mp4", "*.mkv")) {
-#     # Write-Output "file '${file}'"
-#     "file '${file}'" | Out-File -Encoding utf8NoBOM -Append -FilePath .\videos.txt
-# }
+Clear-Content .\videos.txt
+foreach ($file in Get-ChildItem .\videos\* -Include @("*.mp4", "*.mkv")) {
+    # Write-Output "file '${file}'"
+    "file '${file}'" | Out-File -Encoding utf8NoBOM -Append -FilePath .\videos.txt
+}
 # endregion
 
 
 # region Lister tous les audios
 Clear-Content .\audios.txt
-foreach ($file in Get-ChildItem .\musiques\* -Include @("*.wav", "*.mp3")) {
+foreach ($file in Get-ChildItem .\musiques\* -Include @("*.wav")) {
     # Write-Output "file '${file}'"
     "file '${file}'" | Out-File -Encoding utf8NoBOM -Append -FilePath .\audios.txt
 }
@@ -72,27 +71,27 @@ $videoLength = [math]::ceiling($videoLength)
 Write-Output "Longueur vidéo : $videoLength secondes"
 
 #### TIMELAPSE
-
-# $video = ".\videos\Akebi-chan no Sailor-fuku - S01E07 - VOSTFR 1080p WEB x264 -NanDesuKa (WAKA).mkv" ##### À REMPLACER PAR LE TIMELAPSE
+$video = ".\videos\sortie.mkv" ##### À REMPLACER PAR LE TIMELAPSE
 $duration = ffprobe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $video
 
 ### Accélérer la vidéo + fade
-$spedUpLength = ($audioLength - 20)
 $speedUpFactor = $audioLength / $videoLength
 $tempVal = $videoLength - (20/$speedUpFactor)
 Write-Output("fadeOutStart = $fadeOutStart,
-audioLength = $audioLength,
-spedUpLength = $spedUpLength,
 videoLength = $videoLength,
 speedUpFactor = $speedUpFactor")
 
-ffmpeg -y -i $video -t $tempVal -c:v copy .\temp\trimmed.mkv
+#### Cut the number of frames to length desired
+Write-Output "Cutting to length"
+# ffmpeg -y -i $video -t $tempVal -c:v copy .\temp\trimmed.mkv
 
+#### Compute the fade out 
 $frames = ffprobe -v error -select_streams v:0 -count_packets -show_entries stream=nb_read_packets -of csv=p=0 .\temp\trimmed.mkv
 $framerate = [math]::ceiling($frames / $duration)
 $fadeOutStart = ($frames - 1.5 * ($framerate / $speedUpFactor))
 
-ffmpeg -y -i .\temp\trimmed.mkv -vf "setpts=PTS*$speedUpFactor,fade=in:st=0:d=3,fade=out:s=${fadeOutStart}:d=1.5" -r 60 -an -sn -max_interleave_delta 0 .\temp\speed.mkv
+Write-Output "Speeding up and adding fades"
+ffmpeg -y -loglevel error -i .\temp\trimmed.mkv -vf "setpts=PTS*$speedUpFactor,fade=in:st=0:d=3,fade=out:s=${fadeOutStart}:d=1.5" -r 60 -an -sn -max_interleave_delta 0 .\temp\speed.mkv
 
 #### REMUX x265 
 # ffmpeg -y -i $video -vf "setpts=($speedUpFactor)*PTS,fade=in:st=0:d=3,fade=out:s=${fadeOutStart}:d=1.5" -c:v libx265 -an -sn -x265-params crf=17 out.mp4
@@ -125,7 +124,6 @@ if ($testing -eq 1)
 {
     ffmpeg -y -loglevel error -i $image -vf scale=${videoW}*1.02:-1,boxblur=15,eq=brightness=-0.25 .\temp\bottom.jpg
 }
-# ffmpeg -y -loglevel error -i $image -vf scale=${videoW}*0.65:-1 .\temp\top.jpg
 
 ####Squaring out images
 Write-Output("Squaring out images")
@@ -146,10 +144,7 @@ $hiddenHeight = ($videoH - $bottomH) * 0.1
 $pps = $hiddenHeight / 15
 
 Write-Output("Bottom")
-if ($testing -eq 1)
-{
-    ffmpeg -y -loglevel error -loop 1 -i .\temp\bottom.jpg -i .\temp\speed.mkv -filter_complex "[1][0]overlay=(main_w - overlay_w)/2:((main_h - overlay_h)/2 - ${hiddenHeight}) + t*$pps" -r 60 -t 20 .\temp\bottom.mkv
-}
+ffmpeg -y -loglevel error -loop 1 -i .\temp\bottom.jpg -i .\temp\speed.mkv -filter_complex "[1][0]overlay=(main_w - overlay_w)/2:((main_h - overlay_h)/2 - ${hiddenHeight}) + t*$pps" -r 60 -t 20 .\temp\bottom.mkv
 
 
 #### Top
@@ -161,29 +156,28 @@ $hiddenHalf = $hiddenHeight / 2
 $pps = $hiddenHeight / 15
 
 Write-Output("Top")
-if ($testing -eq 1)
-{
-    ffmpeg -y -loglevel error -loop 1 -i .\temp\top.jpg -i .\temp\bottom.mkv -filter_complex "[1][0]overlay=(main_w - overlay_w)/2:((main_h - overlay_h)/2 + $hiddenHalf) - t*$pps,
-    fade=t=in:st=0:d=1.5,
-    fade=t=out:st=17:d=3" -r 60 -t 20 .\temp\overlayed.mkv
-}
+ffmpeg -y -loglevel error -loop 1 -i .\temp\top.jpg -i .\temp\bottom.mkv -filter_complex "[1][0]overlay=(main_w - overlay_w)/2:((main_h - overlay_h)/2 + $hiddenHalf) - t*$pps,
+fade=t=in:st=0:d=1.5,
+fade=t=out:st=17:d=3" -r 60 -t 20 .\temp\overlayed.mkv
 
 #endregion
 
 
 # region Ajouter l'écran de fin à la vidéo
+Write-Output "Concatenating"
 ffmpeg -y -loglevel error -f concat -safe 0 -i .\concat.ffmpeg -c copy .\temp\output.mkv 
 # endregion
 
 
 # region Ajouter la musique
+Write-Output "Adding music"
 ffmpeg -y -loglevel error -f concat -safe 0 -i .\audios.txt -c copy .\temp\output.wav
 
 $audioFadeStart = $audioLength - 5
 
-ffmpeg -y -i .\temp\output.wav -af "afade=t=out:st=${audioFadeStart}:d=5" .\temp\outputFade.wav
+ffmpeg -y -loglevel error -i .\temp\output.wav -af "afade=t=out:st=${audioFadeStart}:d=5" .\temp\outputFade.wav
 
-ffmpeg -y -i .\temp\output.mkv -i .\temp\outputFade.wav -map 0:v -map 1:a -c:v copy -shortest output.mkv
+ffmpeg -y -loglevel error -i .\temp\output.mkv -i .\temp\outputFade.wav -map 0:v -map 1:a -c:v copy output.mkv
 
 ## TODO : problème de longueur du fichier vidéo, à fixer
 # endregion
